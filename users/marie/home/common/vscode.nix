932b12e92798c095221f839453c29e4fceb53b67
charludo <git@charlotteharludo.com>
2025-03-19 11:37:45 +0100

    fix vscode profile definition

--- users/marie/home/common/vscode.nix
+++ users/marie/home/common/vscode.nix
@@ -3,24 +3,26 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-    ];
-    userSettings = {
-      "git.enableSmartCommit" = true;
-      "nix.formatterPath" = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
-      "nix.serverPath" = "${pkgs.nil}/bin/nil";
-      "nix.serverSettings" = {
-        "nil" = {
-          "formatting" = {
-            "command" = [
-              "${pkgs.nixfmt-rfc-style}/bin/nixfmt"
-            ];
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+      ];
+      userSettings = {
+        "git.enableSmartCommit" = true;
+        "nix.formatterPath" = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
+        "nix.serverPath" = "${pkgs.nil}/bin/nil";
+        "nix.serverSettings" = {
+          "nil" = {
+            "formatting" = {
+              "command" = [
+                "${pkgs.nixfmt-rfc-style}/bin/nixfmt"
+              ];
+            };
           };
         };
+        "nix.enableLanguageServer" = true;
+        "editor.formatOnSave" = true;
       };
-      "nix.enableLanguageServer" = true;
-      "editor.formatOnSave" = true;
     };
   };
   home.packages = [
