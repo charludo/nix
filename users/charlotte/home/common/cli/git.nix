a3392a1e8ced0aec7f02f82a054b2854c3b09bb2
charludo <git@charlotteharludo.com>
2025-06-18 17:45:35 +0200

    automatically add git signature to commits

--- users/charlotte/home/common/cli/git.nix
+++ users/charlotte/home/common/cli/git.nix
@@ -13,6 +13,7 @@
         directory = "${config.home.homeDirectory}/Documents";
       };
       commit.gpgsign = true;
+      format.signoff = true;
       gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
       gpg.format = "ssh";
       user.signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
