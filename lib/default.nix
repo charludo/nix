a04a1ebc8f545f29e9d6c8348291656d2afe0d48
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2025-06-23 21:37:23 +0200

    housekeeping: restructure and simplify flake.nix

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,30 @@
-{ lib, ... }:
 {
-  colors = import ./colors.nix { inherit lib; };
-}
+  pkgs,
+  inputs,
+  outputs,
+}:
+
+let
+  mkLib =
+    nixpkgs:
+    nixpkgs.lib.extend (
+      self: _:
+      let
+        lib = self;
+      in
+      {
+        colors = import ./colors.nix { inherit lib; };
+        mkConfigs = import ./mkConfigs.nix {
+          inherit
+            lib
+            pkgs
+            inputs
+            outputs
+            ;
+        };
+      }
+      // inputs.home-manager.lib
+    );
+  lib = mkLib inputs.nixpkgs;
+in
+lib
