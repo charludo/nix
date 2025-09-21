1b862c5704a46466efc56e92245df40e10787d20
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2025-09-21 16:16:39 +0200

    init wanderer

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Pakihöhle <3";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "git+file:///home/charlotte/Projekte/nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
