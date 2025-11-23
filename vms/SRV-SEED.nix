efcd27cd565e54f5bec048db514f03451caf5451
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2025-11-23 10:30:22 +0100

    SRV-SEED: init local fileshare box

---
+++ vms/SRV-SEED.nix
@@ -0,0 +1,27 @@
+{ config, ... }:
+{
+  vm = {
+    id = 3023;
+    name = "SRV-SEED";
+
+    hardware.cores = 2;
+    hardware.memory = 8192;
+    hardware.storage = "32G";
+
+    networking.openPorts.tcp = [ 9000 ];
+    networking.openPorts.udp = [ 9000 ];
+  };
+
+  services = {
+    qbittorrent.enable = true;
+    qbittorrent.openFirewall = true;
+    qbittorrent.webuiPort = 8112;
+  };
+
+  nas.enable = true;
+  nas.extraUsers = [
+    config.services.qbittorrent.user
+  ];
+
+  system.stateVersion = "23.11";
+}
