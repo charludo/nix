70cb1d320f220f2aad2d7e805339e24d5f0d31f5
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2025-11-24 15:19:59 +0100

    hosts/common: fix localization

--- hosts/common/locale.nix
+++ hosts/common/locale.nix
@@ -3,13 +3,14 @@
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
     extraLocaleSettings = {
+      LANG = "en_US.UTF-8";
       LC_TIME = "de_DE.UTF-8";
       LC_MONETARY = "de_DE.UTF-8";
       LC_MEASUREMENT = "de_DE.UTF-8";
       LC_NUMERIC = "de_DE.UTF-8";
       LC_TELEPHONE = "de_DE.UTF-8";
     };
-    supportedLocales = lib.mkDefault [
+    extraLocales = lib.mkDefault [
       "en_US.UTF-8/UTF-8"
       "de_DE.UTF-8/UTF-8"
     ];
