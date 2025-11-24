977356dc149ab7e9c60fa256e0ecb0a37011dff1
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2025-11-24 09:49:16 +0100

    overlays/flake-inputs: fix system -> stdenv.hostPlatform.system warning

--- overlays/flake-inputs.nix
+++ overlays/flake-inputs.nix
@@ -7,15 +7,15 @@
     inputs = builtins.mapAttrs (
       _: flake:
       let
-        legacyPackages = ((flake.legacyPackages or { }).${final.system} or { });
-        packages = ((flake.packages or { }).${final.system} or { });
+        legacyPackages = ((flake.legacyPackages or { }).${final.stdenv.hostPlatform.system} or { });
+        packages = ((flake.packages or { }).${final.stdenv.hostPlatform.system} or { });
       in
       if legacyPackages != { } then legacyPackages else packages
     ) inputs;
   };
   pkgs = (
     _: prev: {
-      ours = outputs.packages.${prev.system};
+      ours = outputs.packages.${prev.stdenv.hostPlatform.system};
     }
   );
 }
