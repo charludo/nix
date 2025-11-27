3a78ed0998c4965e3c4b3ace4d5957a8f471f4f5
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2025-11-27 18:35:33 +0100

    modules/fish: add bash-style prompt

--- modules/nixos/fish.nix
+++ modules/nixos/fish.nix
@@ -16,6 +16,29 @@ in
 
   config = mkIf cfg.enable {
     programs.fish.enable = true;
+    programs.fish.promptInit = # fish
+      ''
+        if not functions -q tide
+          function fish_prompt
+            set -l last_status $status
+            set -l stat
+            if test $last_status -ne 0
+              set stat (set_color red)"[$last_status]"(set_color normal)
+            end
+
+            set -l endprompt
+            if [ (whoami) = "root" ]
+              echo -n (set_color red)
+              set endprompt (set_color red)'# '(set_color normal)
+            else 
+              echo -n (set_color green)
+              set endprompt (set_color green)'$ '(set_color normal)
+            end
+
+            string join ''' -- '[' (whoami) '@' (hostname) ':' (prompt_pwd) ']' $stat $endprompt
+          end
+        end
+      '';
     users.defaultUserShell = pkgs.fish;
   };
 }
