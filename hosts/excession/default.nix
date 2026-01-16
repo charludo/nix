7036d4804befce02567af50872fcfd5cda2b37a3
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2026-01-16 11:11:54 +0100

    hosts/excession: remove duplicate firewall config

--- hosts/excession/default.nix
+++ hosts/excession/default.nix
@@ -62,61 +62,6 @@
   networking.networkmanager.enable = true;
   networking.hostName = "excession";
   networking.nameservers = [ "192.168.30.13" ];
-  networking.firewall = {
-    allowedTCPPorts = [
-      80
-      443
-      4380
-    ];
-    allowedTCPPortRanges = [
-      # Steam
-      {
-        from = 27000;
-        to = 27050;
-      }
-
-      # ESO
-      {
-        from = 24100;
-        to = 24131;
-      }
-      {
-        from = 24300;
-        to = 24331;
-      }
-      {
-        from = 24500;
-        to = 24507;
-      }
-    ];
-    allowedUDPPortRanges = [
-      # Steam
-      {
-        from = 27000;
-        to = 27050;
-      }
-
-      # ESO
-      {
-        from = 24100;
-        to = 24131;
-      }
-      {
-        from = 24300;
-        to = 24331;
-      }
-      {
-        from = 24500;
-        to = 24507;
-      }
-
-      # Stellaris
-      {
-        from = 17780;
-        to = 17785;
-      }
-    ];
-  };
 
   hardware.graphics = {
     enable = true;
