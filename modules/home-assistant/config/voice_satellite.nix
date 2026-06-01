23ccd776b2ddd110aa0510c5b2b0727021a6a040
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2026-06-01 15:59:07 +0200

    soft error sound

--- modules/home-assistant/config/voice_satellite.nix
+++ modules/home-assistant/config/voice_satellite.nix
@@ -31,7 +31,7 @@ in
     timer = ./assets/sounds/timer-bell.mp3;
     reminder = ./assets/sounds/reminder-friendly.mp3;
     alarmclock = ./assets/sounds/wecker-chipper.mp3;
-    error = ./assets/sounds/error-kick.mp3;
+    error = ./assets/sounds/error-soft.mp3;
     duck = ./assets/sounds/silence.mp3;
   };
 }
