4511eb96933219525178a78a20f98bff0a9dc9a8
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2026-09-08 11:58:56 +0200

    SRV-DAWARICH: configure OIDC via PocketID

--- vms/SRV-DAWARICH.nix
+++ vms/SRV-DAWARICH.nix
@@ -23,11 +23,22 @@
     configureNginx = true;
     environment = {
       APPLICATION_PROTOCOL = "https";
+
+      OIDC_ISSUER = "https://sso.${private-settings.domains.home}";
+      OIDC_PROVIDER_NAME = "Pocket ID";
+      OIDC_REDIRECT_URI = "https://dawarich.${private-settings.domains.home}/users/auth/openid_connect/callback";
+      OIDC_AUTO_REGISTER = "true";
+      ALLOW_EMAIL_PASSWORD_REGISTRATION = "false";
+
+      # Set to true for access to the admin account.
+      ALLOW_EMAIL_PASSWORD_LOGIN = "false";
     };
 
     secretKeyBaseFile = config.age.secrets.dawarich.path;
+    extraEnvFiles = [ config.age.secrets.dawarich-oidc.path ];
   };
   age.secrets.dawarich.rekeyFile = secrets.dawarich;
+  age.secrets.dawarich-oidc.rekeyFile = secrets.dawarich-oidc;
 
   services.nginx.virtualHosts.${config.services.dawarich.localDomain}.locations."@proxy" = {
     recommendedProxySettings = lib.mkForce false;
